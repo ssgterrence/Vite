dc0eca835b1949b7dca8eb596f8ed99aaf0adea0
TerrenceShi <eu15@18m.dev>
2023-07-14 10:12:14 +0800

    fix: change navigation bar to hamburger when screen changes

--- src/components/TheNewYorkTimes.vue
+++ src/components/TheNewYorkTimes.vue
@@ -22,13 +22,22 @@
     <div class="navigationBar">
       LoremipsumdolorsitametconsecteturadipisicingelitMolestiasaperiamducimusquiaLoremipsumdolorsitametconsecteturadipisicingelitMolestiasaperia
     </div>
+    <div class="hamburger">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
     <div class="leftContent">
       Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores, dolores! Sed facere aliquam
       nobis, vel natus excepturi exercitationem eaque eum mollitia quod laborum, maxime laboriosam,
       praesentium animi nam! Perferendis, saepe?
     </div>
     <div class="middleContent"></div>
-    <div class="rightContent"></div>
+    <div class="rightContent">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis maxime corporis explicabo ipsa
+      ab dolorem veniam enim assumenda, quos magni nesciunt nemo ea sit error rerum sequi
+      exercitationem hic delectus?
+    </div>
   </div>
 </template>
 <script>
@@ -38,8 +47,8 @@ export default {}
 .gridContainer {
   gap: 10px;
   display: grid;
-  margin-left: 200px;
-  margin-right: 200px;
+  margin-left: 100px;
+  margin-right: 100px;
   grid-template-columns: repeat(12, 1fr);
 }
 .header {
@@ -63,13 +72,35 @@ export default {}
   grid-column: -4/-1;
   background-color: darkcyan;
 }
-
+.hamburger {
+  display: none;
+  cursor: pointer;
+}
+.hamburger div {
+  width: 25px;
+  height: 2px;
+  background-color: #333;
+  margin: 3px 0;
+}
+@media screen and (max-width: 1348px) {
+  .navigationBar {
+    display: none;
+  }
+  .hamburger {
+    display: grid;
+    grid-column: span 12;
+  }
+}
 @media screen and (max-width: 999px) {
   .leftContent {
     grid-column: span 5;
   }
   .middleContent {
     grid-column: -8/-1;
+  }
+  .rightContent {
+    grid-column: span 12; /* spans across all columns */
+    background-color: darkcyan;
   }
 }
 </style>
